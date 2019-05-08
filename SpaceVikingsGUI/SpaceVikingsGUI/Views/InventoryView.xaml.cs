bcfa57565d1dde734f0df7e79227d0dd30c723a1
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-05-09 00:37:47 +0200

    WIP

--- SpaceVikingsGUI/SpaceVikingsGUI/Views/InventoryView.xaml.cs
+++ SpaceVikingsGUI/SpaceVikingsGUI/Views/InventoryView.xaml.cs
@@ -24,5 +24,10 @@ namespace SpaceVikingsGUI.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
