9afabe2068d27d52792547f006397b79cc517c1c
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-05-14 12:04:02 +0200

    XUNIT test of UI WIP

--- SpaceVikingsGUI/SpaveVikingsGUI.test/UnitTest1.cs
+++ SpaceVikingsGUI/SpaveVikingsGUI.test/UnitTest1.cs
@@ -1,19 +1,37 @@
+//using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using Xunit;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
 
 namespace Tests
 {
     public class LoginTest
     {
-        [SetUp]
-        public void Setup()
+        [Fact]
+        public void GettingStarted()
         {
-            
+            Application application =
+                Application.Launch(
+                    @"D:\Skoli\Skoli\4.SEMESTER\PRJ4\GUI_V2\SpaceVikingsGUI\SpaceVikingsGUI\bin\Debug\SpaceVikingsGUI.exe");
+            Window main = application.GetWindow("MainWindow");
+            main.Get<TextBox>(SearchCriteria.Indexed(0)).Text = "test";
+            var pass = main.Get<TextBox>(SearchCriteria.Indexed(1));
+            pass.BulkText = "string";
+            //main.Get<Button>(SearchCriteria.ByText("Login")).Click();
+            Assert.That(pass.BulkText,Is.EqualTo("string"));
+                
         }
+        
+        
 
-        [Test]
-        public void AuthenicUser()
-        {
-            Assert.Pass();
-        }
+
     }
 }
